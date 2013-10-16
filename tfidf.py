9956a1eaeab0b351e3914e815a6266fb54f63e84
root <root@ip-10-139-0-96.ec2.internal>
2013-10-16 22:43:17 +0000

    finished basic tfidf

--- tfidf.py
+++ tfidf.py
@@ -10,11 +10,14 @@ sc = SparkContext("spark://ec2-107-22-0-110.compute-1.amazonaws.com:7077", "TF-I
 
 from term_tools import get_terms
 
+# Returns a list of dicts => each dict containing a sender and a term
 def sender_term_pairs(email):
   sender = email['sender']
   return map(lambda x: {'sender': sender, 'term': x}, get_terms(email['text']))
 
+# [{'term': u'talk', 'sender': u'rosalee.fleming@enron.com'}, {'term': u'talk', 'sender': u'rosalee.fleming@enron.com'}, {'term': u'talk', 'sender': u'rosalee.fleming@enron.com'}]
 
+# Given a list like the one above,  creates a dictionary of the frequency of the term by the sender 
 def single_sender_term_freq(sender_list):
   sender_tf = {}
   for pair in sender_list:
@@ -25,19 +28,17 @@ def single_sender_term_freq(sender_list):
         sender_tf[key] = 1
 
   return sender_tf
-
-#(u'talk', [{'term': u'talk', 'sender': u'rosalee.fleming@enron.com'}, {'term': u'talk', 'sender': u'rosalee.fleming@enron.com'}, {'term': u'talk', 'sender': u'rosalee.fleming@enron.com'}])
-  
+ 
+# Given a tuple of a term and a list of individual occurences by different senders, calculates the term frequency per sender
 def sender_tf(grouped_pair):
-  # Calculate the term frequency for the term-grouped pair
   tf_dict = single_sender_term_freq(grouped_pair[1])
   term = grouped_pair[0]
 
   return map(lambda y: (term, y), tf_dict.items())
 
 
-#corpus = sc.textFile('s3n://AKIAJFDTPC4XX2LVETGA:lJPMR8IqPw2rsVKmsSgniUd+cLhpItI42Z6DCFku@6885public/enron/lay-k.json')
-corpus = sc.textFile('s3n://AKIAJFDTPC4XX2LVETGA:lJPMR8IqPw2rsVKmsSgniUd+cLhpItI42Z6DCFku@6885public/fsosa/short.json')
+corpus = sc.textFile('s3n://AKIAJFDTPC4XX2LVETGA:lJPMR8IqPw2rsVKmsSgniUd+cLhpItI42Z6DCFku@6885public/enron/lay-k.json')
+#corpus = sc.textFile('s3n://AKIAJFDTPC4XX2LVETGA:lJPMR8IqPw2rsVKmsSgniUd+cLhpItI42Z6DCFku@6885public/fsosa/short.json')
 
 json_corpus = corpus.map(lambda x: json.loads(x)).cache()
 
@@ -49,8 +50,10 @@ per_term_idf = term_counts.map(lambda x: (x[0], math.log(516893.0 / x[1]))).cach
 grouped_sender_term_pairs = json_corpus.flatMap(sender_term_pairs).groupBy(lambda x: x['term'])
 
 # Calculate sender-term frequency
-sender_tf = grouped_sender_term_pairs.flatMap(sender_tf)
+sender_tf = grouped_sender_term_pairs.flatMap(sender_tf).cache()
 
-output = sender_tf.collect()
-for x in output: 
-   print x
+#(u'talk', ((u'rosalee.fleming@enron.com', 3), 12.056978880153091))
+tfidf = sender_tf.join(per_term_idf).map(lambda x:{'sender': x[1][0][0], 'term':x[0], 'tf-idf':x[1][0][1]*x[1][1]}).collect()
+
+for x in tfidf:
+  print x
