7d7b4858be8feb4bc7c1c6878f93e186d8e8f53f
root <root@ip-10-139-0-96.ec2.internal>
2013-10-17 00:52:59 +0000

    added comments, bit of diambiguation

--- tfidf.py
+++ tfidf.py
@@ -16,7 +16,7 @@ def sender_term_pairs(email):
   return map(lambda x: {'sender': sender, 'term': x}, get_terms(email['text']))
 
 # [{'term': u'talk', 'sender': u'rosalee.fleming@enron.com'}, {'term': u'talk', 'sender': u'rosalee.fleming@enron.com'}, {'term': u'talk', 'sender': u'rosalee.fleming@enron.com'}]
-
+#
 # Given a list like the one above,  creates a dictionary of the frequency of the term by the sender 
 def single_sender_term_freq(sender_list):
   sender_tf = {}
@@ -37,11 +37,22 @@ def sender_tf(grouped_pair):
   return map(lambda y: (term, y), tf_dict.items())
 
 
+
+#---- BEGIN PROCESSING ------#
 corpus = sc.textFile('s3n://AKIAJFDTPC4XX2LVETGA:lJPMR8IqPw2rsVKmsSgniUd+cLhpItI42Z6DCFku@6885public/enron/lay-k.json')
 #corpus = sc.textFile('s3n://AKIAJFDTPC4XX2LVETGA:lJPMR8IqPw2rsVKmsSgniUd+cLhpItI42Z6DCFku@6885public/fsosa/short.json')
 
 json_corpus = corpus.map(lambda x: json.loads(x)).cache()
 
+
+#--- Disambiguation ---#
+unique_emails = json_corpus.map(lambda x: x['sender']).distinct()
+lastnames = unique_emails.flatMap(lambda x: (x, x.split('.')) ).filter(lambda y: len(y[1]) != 2)
+
+output = lastnames.collect()
+
+
+#----- Actual TF-IDF Calculation -----#
 # Calculate per-term idf
 term_counts = json_corpus.flatMap(lambda x: get_terms(x['text'])).map(lambda y: (y, 1)).reduceByKey(add)
 per_term_idf = term_counts.map(lambda x: (x[0], math.log(516893.0 / x[1]))).cache()
@@ -52,8 +63,9 @@ grouped_sender_term_pairs = json_corpus.flatMap(sender_term_pairs).groupBy(lambd
 # Calculate sender-term frequency
 sender_tf = grouped_sender_term_pairs.flatMap(sender_tf).cache()
 
-#(u'talk', ((u'rosalee.fleming@enron.com', 3), 12.056978880153091))
-tfidf = sender_tf.join(per_term_idf).map(lambda x:{'sender': x[1][0][0], 'term':x[0], 'tf-idf':x[1][0][1]*x[1][1]}).collect()
+#e.g. join: (u'talk', ((u'rosalee.fleming@enron.com', 3), 12.056978880153091))
+tfidf = sender_tf.join(per_term_idf).map(lambda x:{'sender': x[1][0][0], 'term':x[0], 'tf-idf':x[1][0][1]*x[1][1]})
 
-for x in tfidf:
+#output = tfidf.collect()
+for x in output:
   print x
